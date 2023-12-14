bc7645639a1091740228ed3d2923696e4628d24e
= <rubensglf@gmail.com>
2023-12-14 12:32:00 -0300

    structuring header and make menu button work properly

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,2 +1,16 @@
+// Import files
 import style from '../../public/css/style.css'
-import Todo from '../todo.js'
+import Todo from '../scripts/components/todo.js'
+
+// Declare variables
+const header = document.querySelector('.header')
+const sidebar = document.querySelector('.sidebar')
+const main = document.querySelector('.main')
+const toggleBtn = document.querySelector('.toggle-sidebar')
+
+// Toggle Sidebar Menu
+toggleSidebar.addEventListener('click', () => {
+    sidebar.hidden = true
+    main.style.width = "100vw"
+    // TODO
+})
