87436a8765a80c30e00f0085fcb5510f4d952084
= <rubensglf@gmail.com>
2023-12-27 15:35:37 -0300

    fix small screen size menu bug and add colors to the project

--- src/components/toggle-menu.js
+++ src/components/toggle-menu.js
@@ -1,26 +1,19 @@
+const header = document.querySelector('.header')
 const main = document.querySelector('.main')
 const menu = document.querySelector('.menu')
 const toggleMenuBtn = document.querySelector('.toggle-menu-btn')
-const arrow = document.querySelectorAll('.header-icons')[0]
 
 // Alternar em mostrar ou não mostrar o conteúdo do menu
 const toggleMenu = (() => {
     toggleMenuBtn.addEventListener('click', () => {
         if (menu.style.display === 'none') {
             menu.style.display = 'flex'
-            arrow.classList.add('fa-arrow-left')
-            arrow.classList.remove('fa-bars')
-            if (menu.style.width = '20vw') {
-                main.style.width = '80vw'
-            }
         } else {
             menu.style.display = 'none'
-            arrow.classList.remove('fa-arrow-left')
-            arrow.classList.add('fa-bars')
+            header.style.width = '100vw'
             main.style.width = '100vw'
-            menu.style.width = 'none'
         }
     })
 })()
 
-export default toggleMenu;
+export default toggleMenu
