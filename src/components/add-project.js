5f6bc6c99bf200cf566e5c09b34b955f7f1ecfab
= <rubensglf@gmail.com>
2024-01-08 16:03:35 -0300

    update header design

--- src/components/add-project.js
+++ src/components/add-project.js
@@ -2,6 +2,7 @@ const projectNameInput = document.querySelector('.project-name-input')
 const addProjectBtn = document.querySelector('.add-project-btn')
 const bottomMenuContent = document.querySelector('.bottom-menu-content')
 const allProjectsTxt = document.querySelector('.all-projects-txt')
+let projectTitle;
 
 function capitalize(s) {
     return s && s[0].toUpperCase() + s.slice(1);
@@ -9,11 +10,11 @@ function capitalize(s) {
 
 const addProject = (() => {
     addProjectBtn.addEventListener('click', () => {
-        // do not add if the input is empty
+        // Do not add if the input is empty
         if (!projectNameInput.value || projectNameInput.value === ' ') {
             alert('Não é póssivel adicionar um projeto sem um nome.')
             return
-            // if not empty, add
+            // Add if not empty
         } else {
             bottomMenuContent.insertAdjacentHTML('beforeend',
                 `<div class="project">
@@ -21,12 +22,22 @@ const addProject = (() => {
                     <i class="fa-solid fa-delete-left"></i>
                 </div>`
             )
+            // Get Project title
+            const btns = document.querySelectorAll('.project-name-btn')
+            for (let i = 0; i < btns.length; i++) {
+                projectTitle = btns[i].textContent
+            }
+            console.log(projectTitle)
             allProjectsTxt.hidden = false
             projectNameInput.value = ''
             projectNameInput.type = 'reset'
             projectNameInput.type = 'text'
         }
     })
+})()
+
+const genaratePage = (() => {
+
 })()
 
 
